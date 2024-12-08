5840738c597b8075cc62d558e30920a617c3c3db
tao at lab <taoshao213@gmail.com>
2024-12-08 21:49:41 +0800

    fix draw utils

--- utils/draw_utils.py
+++ utils/draw_utils.py
@@ -13,6 +13,9 @@ from utils.file_utils import save_image
 class default_draw_config:
     line_color = (255, 165, 0)
     line_thickness = 10
+
+    box_color = (255, 165, 0)
+    box_thickness = 10
     
     arrow_color = (255, 165, 0)
     arrow_thickness = 10
@@ -44,6 +47,23 @@ def transparent_decorator(alpha: float = default_draw_config.transparent_alpha):
     return decorator
 
 
+def float_cord_to_int_xy(pt, img: np.ndarray):
+    height, width = img.shape[:2]
+    if len(pt) == 2:
+        x, y = pt
+        x = int(x * width)
+        y = int(y * height)
+        return x, y
+    elif len(pt) == 4:
+        x1, y1, x2, y2 = pt
+        x1 = int(x1 * width)
+        y1 = int(y1 * height)
+        x2 = int(x2 * width)
+        y2 = int(y2 * height)
+        return x1, y1, x2, y2
+    else:
+        raise ValueError('Invalid input') 
+
 
 def show_image(img: np.ndarray):
     plt.imshow(img)
@@ -58,6 +78,7 @@ def draw_dot(img: np.ndarray,
              radius=default_draw_config.dot_radius, 
              thickness=default_draw_config.dot_thickness):
     img_ = deepcopy(img)
+    pt = float_cord_to_int_xy(pt, img)
     cv2.circle(img_, pt, radius, color, thickness)
     return img_
 
@@ -69,15 +90,17 @@ def draw_arrow(img: np.ndarray,
                thickness=default_draw_config.arrow_thickness, 
                tipLength=default_draw_config.arrow_tipLength):
     img_ = deepcopy(img)
+    pt = float_cord_to_int_xy(pt, img)
     cv2.arrowedLine(img_, pt1, pt2, color, thickness, tipLength=tipLength)
     return img_
 
 @transparent_decorator()
 def draw_box(img: np.ndarray, 
              box: Tuple[float, float, float, float], 
-             color=default_draw_config.seeclick_box_color, 
-             thickness=default_draw_config.seeclick_box_thickness):
+             color=default_draw_config.box_color, 
+             thickness=default_draw_config.box_thickness):
     img_ = img.copy()
+    box = float_cord_to_int_xy(box, img)
     cv2.rectangle(img_, (box[0], box[1]), (box[2], box[3]), color, thickness)
     return img_
 
