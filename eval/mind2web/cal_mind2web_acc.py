e0f71d799395ce8c793e6bc03defb0ca17c5a79b
tao at lab <taoshao213@gmail.com>
2024-12-12 11:31:16 +0800

    val scripts for mind2web

---
+++ eval/mind2web/cal_mind2web_acc.py
@@ -0,0 +1,149 @@
+import os
+import sys
+PROJECT_ROOT = os.path.dirname(os.path.abspath('..'))
+sys.path.append(PROJECT_ROOT)
+
+import logging
+import numpy as np
+import json
+import pandas as pd
+import tabulate
+import argparse
+
+from utils.file_utils import read_json
+from utils.helper_utils import print_args
+
+
+
+def calculate_f1(pred, label):
+    """calculate action f1 following mind2web
+    """
+    pred = set(pred.strip().split())
+    label = set(label.strip().split())
+    if len(pred) == 0 and len(label) == 0:
+        return 1
+    if len(pred) == 0 or len(label) == 0:
+        return 0
+
+    tp = len(pred & label)
+    fp = len(pred - label)
+    fn = len(label - pred)
+    precision = tp / (tp + fp)
+    recall = tp / (tp + fn)
+    if precision == 0 or recall == 0:
+        return 0
+    f1 = 2 * precision * recall / (precision + recall)
+    return f1
+
+def parse_model_output(output_string):
+    try:
+        out = json.loads(output_string)
+        # action and pt is a must in mind2web
+        action = out['action']
+        pt = out['pt']
+        text = out.get('text', '')
+    except Exception as e:
+        print(f"Error in parsing model output: {output_string}")
+        # print(e)
+        action, pt, text = 'error', [-1, -1], ''
+    return action, pt, text
+
+
+def get_metrices(row):
+    try:
+        pred_action, pred_pt, pred_text = row['pred_action'], row['pred_pt'], row['pred_text']
+        gt_action, gt_box, gt_text = row['gt_action'], row['gt_box'], row['gt_text']
+        if pred_action == gt_action:
+            row['Op_match'] = True
+        # NOTE: all action in mind2web need output box
+        # NOTE: maybe a bug, ele_match do not consider whether action is match
+        if (gt_box[0] <= pred_pt[0] <= gt_box[2]) and (gt_box[1] <= pred_pt[1] <= gt_box[3]):
+            row['Ele_match'] = True
+                
+        pred_str = pred_action
+        if pred_action == 'select' or pred_action == 'type':
+            pred_str += ' '
+            pred_str += pred_text.lower()
+        ref_str = gt_action
+        if gt_action == 'select' or gt_action == 'type':
+            ref_str += ' '
+            # NOTE: to compensate for problem in making data, need to fix later
+            ref_str += gt_text.lower().replace('"', "'")
+        f1 = calculate_f1(pred_str, ref_str)
+        row['Op_f1'] = f1
+        if f1 == 1.0 and row['Ele_match'] == True:
+            row['step_success'] = 1.0
+    
+    except Exception as e:
+        logging.error(f"Error in matching action: {pred_action}, {gt_action}")
+        logging.error(e)
+    return row
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate action matching results.')
+    parser.add_argument('--data_path', type=str, required=True, help='Path to the JSON data file.')
+    args = parser.parse_args()
+    print_args(args)
+
+    data_p = args.data_path
+    data = read_json(data_p)
+
+    # make df
+    res = []
+    for sample in data:
+        img_p = sample['image_lst'][0]
+        assert sample['conversation'][1]['from'] == 'gpt'
+        assert sample['conversation'][2]['from'] == 'gt'
+        assert sample['conversation'][3]['from'] == 'prediction'
+        gt = sample['conversation'][2]['value']
+        pred = sample['conversation'][3]['value']
+
+        try:
+            gt_action, gt_box, gt_text = gt['action_type'], gt['box'], gt['text']
+            gt_box = [int(x) for x in gt_box]
+            pred_action, pred_pt, pred_text = parse_model_output(pred)
+            assert pred_action.upper() in ['ERROR', 'SCROLL', 'CLICK', 'TYPE', 'TASK_COMPLETE', 'SELECT']
+        except:
+            logging.error(f"Error in parsing action: {gt}, {pred}")
+            continue
+        
+        match_res = {}
+        match_res['img_meta'] = img_p
+        match_res['ann_id'] = gt['ann_id']
+        match_res['pred_action'] = pred_action.strip()
+        match_res['pred_pt'] = pred_pt
+        match_res['pred_text'] = pred_text.strip()
+        match_res['gt_action'] = gt_action.strip()
+        match_res['gt_box'] = gt_box
+        match_res['gt_text'] = gt_text.strip()
+        res.append(match_res)
+
+
+    # cal acc
+    df = pd.DataFrame(res)
+    df['Op_match'] = False
+    df['Ele_match'] = False 
+    df['Op_f1'] = 0
+    df['step_success'] = 0
+    
+    df = df.apply(get_metrices, axis=1)
+    step_sr = df['step_success'].mean()
+    op_match_acc = df['Op_match'].mean()
+    ele_acc = df['Ele_match'].mean()
+    op_f_lst = []
+    for g_name, g_df in df.groupby('ann_id'):
+        g_f1 = g_df['Op_f1'].mean()
+        op_f_lst.append(g_f1)
+    op_f1 = np.mean(op_f_lst)
+
+    table = [['ele_acc', ele_acc],
+            ['op_f1', op_f1],
+            ['step_success', step_sr], 
+            ['op_match_acc', op_match_acc]]
+    print(tabulate.tabulate(table, tablefmt='grid'))
+
+
+
+
+
---
+++ eval/mind2web/request_vllm_qwenvl_mind2web.py
@@ -0,0 +1,127 @@
+import os
+import sys
+PROJECT_ROOT = os.path.dirname(os.path.abspath('..'))
+sys.path.append(PROJECT_ROOT)
+
+from openai import OpenAI
+import logging
+from tqdm import tqdm
+import cv2
+from copy import deepcopy
+import argparse
+from concurrent.futures import ThreadPoolExecutor
+
+
+from utils.file_utils import (read_image, 
+                              save_image, 
+                              read_json, 
+                              save_json, 
+                              get_image_base64)
+from utils.img_ops import resize_image_short_side
+from utils.helper_utils import smart_resize, print_args
+
+
+def infer(client, model_name, prompt, img_p, temprature):
+    image_np = read_image(img_p)
+    h, w = image_np.shape[:2]
+    if use_smart_resize:
+        h, w = smart_resize(h, w, max_pixels=max_img_tokens * 28 * 28)
+        image_np = cv2.resize(image_np, (w, h))
+    else:
+        image_np = resize_image_short_side(image_np, img_short_side_size)
+    
+    chat_response = client.chat.completions.create(
+        model=model_name,
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": get_image_base64(image_np),
+                        },
+                    },
+                    {"type": "text", "text": prompt},
+                ],
+            },
+        ],
+        temperature=temprature,
+        seed=42
+    )
+    model_pred = chat_response.choices[0].message.content
+    return model_pred
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Process some parameters.')
+    parser.add_argument('--api_key', type=str, default="shaotao", help='OpenAI API key')
+    parser.add_argument('--api_base', type=str, default="http://localhost:8001/v1", help='OpenAI API base URL')
+    parser.add_argument('--debug', action='store_true', help='Enable debug mode')
+    parser.add_argument('--temprature', type=float, default=0.3, help='Temperature')
+    parser.add_argument('--num_thread', type=int, default=20, help='Number of threads')
+    
+    parser.add_argument('--model_name', type=str, required=True, help='Model name')
+    parser.add_argument('--inp_json_p', type=str, required=True, help='Input JSON path')
+    parser.add_argument('--out_json_p', type=str, required=True, help='Output JSON file path')
+    parser.add_argument('--img_short_side_size', type=int, default=-1, help='Image short side size')
+    parser.add_argument('--max_img_tokens', type=int, help='Maximum number of image tokens')
+    parser.add_argument('--use_smart_resize', action='store_true', help='Use smart resize for images')
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = get_args()
+    print_args(args)
+    openai_api_key = args.api_key
+    openai_api_base = args.api_base
+    model_name = args.model_name
+    inp_json_p = args.inp_json_p
+    out_json_p = args.out_json_p
+    debug = args.debug
+    img_short_side_size = args.img_short_side_size
+    temprature = args.temprature
+    num_thread = args.num_thread
+    max_img_tokens = args.max_img_tokens
+    use_smart_resize = args.use_smart_resize
+    
+    if use_smart_resize:
+        assert img_short_side_size == -1, "Cannot use both smart resize and fixed short side size"
+    
+
+    client = OpenAI(
+            api_key=openai_api_key,
+            base_url=openai_api_base,
+        )
+    
+    data = read_json(inp_json_p)
+    os.makedirs('out', exist_ok=True)
+    out_json_p = f'out/{out_json_p}'
+    
+    # pack all params into list
+    params = [(client, 
+                model_name,
+                data[d_idx]['conversation'][0]['value'].replace('<image>\n', ''), 
+                data[d_idx]['image_lst'][0],
+                temprature) for d_idx in range(len(data))]
+    if debug:
+        params = params[:20]
+    
+    
+    with ThreadPoolExecutor(max_workers=num_thread) as executor:
+        # use map to keep the order of results
+        results = list(tqdm(executor.map(lambda param: infer(*param), 
+                                         params),
+                            total=len(params)))
+
+        
+    out = []
+    for d_idx, model_pred in enumerate(results):
+        out_line = deepcopy(data[d_idx])
+        out.append(out_line)
+        out[d_idx]['conversation'].append({'from': 'prediction', 
+                                           'value': model_pred})
+    if not debug:
+        save_json(out, out_json_p)
+
